2b8ab163e0adcb7a6574626d79d33ff7f01944f6
yuhaixy <yuhaixy@gmail.com>
2018-06-01 13:49:16 +0800

    mod

--- src/huwo_jt808_processor.erl
+++ src/huwo_jt808_processor.erl
@@ -2,10 +2,32 @@
 
 -export([process_frame/2]).
 
-process_frame(Frame, PState) ->
-    Type = 1,
-    process_request(Type, Frame, PState),
-    ok.
 
+%% 如果不是注册设备的消息，但状态中的connection没定义说明没有注册就发送其他信息
+process_frame(#huwo_jt808_frame{ header = #huwo_jt808_frame_header{ id = MsgId}},
+              PState = #proc_state{ connection = undefined }) %%
+            when MsgId =/= ?MSG_ID_REG ->
+                {error, connect_expected, PState};
+%% 开始处理包
+rocess_frame(Frame = #huwo_jt808_frame{ header = #huwo_jt808_frame_header{ id = MsgId }},
+              PState) ->
+    case process_request(MsgId, Frame, PState) of
+        {ok, PState1} -> {ok, PState1, PState1#proc_state.connection};
+        Ret -> Ret
+    end.
+%process_frame(Frame, PState) ->
+%    Type = 1,
+%    process_request(Type, Frame, PState),
+%    ok.
+
+process_request(?MSG_ID_REG,
+                Frame,
+                PState0 = #proc_state{ ssl_login_name = SSLLoginName,
+                                       send_fun       = SendFun,
+                                       adapter_info   = AdapterInfo = #amqp_adapter_info{additional_info = Extra} }) ->
+    % TODO: 验证登录
+    % TODO: 给当前用户开推送队列，用于服务端推送
+    bin_utils:dump(process_request, Frame),
+    {ok, PState1};
 process_request(_Type, Frame, _PState) ->
     bin_utils:dump(process_request, Frame).
--- src/huwo_jt808_reader.erl
+++ src/huwo_jt808_reader.erl
@@ -1,7 +1,53 @@
--module(huwo_jt808_reader).
+%% gen_server work module
 
--export([process_received_bytes/2]).
--export([parse/2]).
+-module(huwo_jt808_reader).
+-behaviour(gen_server2).
+
+-export([start_link/3]).
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2,
+         code_change/3, terminate/2]).
+
+-export([conserve_resources/3, start_keepalive/2]).
+
+-export([info/2]).
+
+-include_lib("amqp_client/include/amqp_client.hrl").
+-include("rabbit_mqtt.hrl").
+
+
+% used for debug
+-export([process_received_bytes/2, parse/2]).
+
+
+start_link(KeepaliveSup, Ref, Sock) ->
+    Pid = proc_lib:spawn_link(?MODULE, init,
+                              [[KeepaliveSup, Ref, Sock]]),
+
+    %% In the event that somebody floods us with connections, the
+    %% reader processes can spew log events at error_logger faster
+    %% than it can keep up, causing its mailbox to grow unbounded
+    %% until we eat all the memory available and crash. So here is a
+    %% meaningless synchronous call to the underlying gen_event
+    %% mechanism. When it returns the mailbox is drained, and we
+    %% return to our caller to accept more connections.
+    gen_event:which_handlers(error_logger),
+
+    {ok, Pid}.
+
+conserve_resources(Pid, _, {_, Conserve, _}) ->
+    Pid ! {conserve_resources, Conserve},
+    ok.
+
+% keepalive
+start_keepalive(_,   0        ) -> ok;
+start_keepalive(Pid, Keepalive) -> Pid ! {start_keepalives, Keepalive}.
+
+info(Pid, InfoItems) ->
+    case InfoItems -- ?INFO_ITEMS of
+        [] -> gen_server2:call(Pid, {info, InfoItems});
+        UnknownItems -> throw({bad_argument, UnknownItems})
+    end.
+%%----------------------------------------------------------------------------
 
 process_received_bytes(Bytes, State) ->
     case parse(Bytes, State) of
