75453de959391124e2ce303506480f7efea2275d
Luo Tao <lotreal@gmail.com>
2018-06-07 16:43:18 +0800

    fork script from https://github.com/rabbitmq/rabbitmq-tutorials/tree/master/erlang

---
+++ scripts/emit_log_topic.erl
@@ -0,0 +1,31 @@
+#!/usr/bin/env escript
+%%! -pz ./amqp_client ./rabbit_common ./amqp_client/ebin ./rabbit_common/ebin ./recon/ebin
+
+-include_lib("amqp_client/include/amqp_client.hrl").
+
+main(Argv) ->
+    {ok, Connection} =
+        amqp_connection:start(#amqp_params_network{host = "localhost"}),
+    {ok, Channel} = amqp_connection:open_channel(Connection),
+
+    amqp_channel:call(Channel, #'exchange.declare'{exchange = <<"amq.topic">>,
+                                                   durable=true,
+                                                   type = <<"topic">>}),
+
+    {RoutingKey, Message} = case Argv of
+                                [] ->
+                                    {<<"topic">>, <<"Hello World!">>};
+                                [R] ->
+                                    {list_to_binary(R), <<"Hello World!">>};
+                                [R | Msg] ->
+                                    {list_to_binary(R), list_to_binary(string:join(Msg, " "))}
+                            end,
+    amqp_channel:cast(Channel,
+                      #'basic.publish'{
+                        exchange = <<"amq.topic">>,
+                        routing_key = RoutingKey},
+                      #amqp_msg{payload = Message}),
+    io:format(" [x] Sent ~p:~p~n", [RoutingKey, Message]),
+    ok = amqp_channel:close(Channel),
+    ok = amqp_connection:close(Connection),
+    ok.
